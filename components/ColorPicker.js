9826919b3c8876baaac4a6ef05dbdde262a39a4f
Shelley McHardy <shelleymcq.dev@gmail.com>
2023-06-26 12:08:17 -0400

    fea: save selected color to chosen save slot

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,26 +1,62 @@
 import React, { useState } from "react";
 import { HexColorPicker } from "react-colorful";
 
+const savedColorsDefault = [
+  {
+    id: 0,
+    hex: "#eeeeee",
+  },
+  {
+    id: 1,
+    hex: "#eeeeee",
+  },
+  {
+    id: 2,
+    hex: "#eeeeee",
+  },
+  {
+    id: 3,
+    hex: "#eeeeee",
+  },
+  {
+    id: 4,
+    hex: "#eeeeee",
+  },
+  {
+    id: 5,
+    hex: "#eeeeee",
+  },
+  {
+    id: 6,
+    hex: "#eeeeee",
+  },
+  {
+    id: 7,
+    hex: "#eeeeee",
+  },
+  {
+    id: 8,
+    hex: "#eeeeee",
+  },
+  {
+    id: 9,
+    hex: "#eeeeee",
+  },
+];
+
 const ColorPicker = () => {
   const [color, setColor] = useState("#000000");
+  const [savedColors, setSavedColors] = useState(savedColorsDefault);
 
-  const temperatureBands = [
-    {
-      low: 11,
-      high: 20,
-      color: "#b49be6",
-    },
-    {
-      low: 21,
-      high: 30,
-      color: "#c16060",
-    },
-    {
-      low: 31,
-      high: 40,
-      color: "#49c13c",
-    },
-  ];
+  const handleSave = (idToUpdate, hexToUpdate) => {
+    setSavedColors(
+      savedColors.map((previousColor) =>
+        previousColor.id === idToUpdate
+          ? { ...previousColor, hex: hexToUpdate }
+          : previousColor
+      )
+    );
+  };
 
   return (
     <div className="mt-10 flex">
@@ -29,18 +65,27 @@ const ColorPicker = () => {
         <p style={{ backgroundColor: `${color}` }}>{color}</p>
       </div>
       <div>
-        {temperatureBands.map((band, index) => (
-          <div className="flex" key={index}>
-            <p className="px-2">{band.low}</p>
-            <p className="px-2">{band.high}</p>
-            <p
-              style={{ backgroundColor: band.color }}
-              className="m-1 px-2 w-40"
-            >
-              {band.color}
-            </p>
-          </div>
-        ))}
+        <h3>Saved Colors</h3>
+        <div>
+          {savedColors.map((item) => (
+            <div>
+              <p
+                style={{ backgroundColor: `${item.hex}` }}
+                key={item.id}
+                className="m-2 w-30"
+                onClick={() => handleSave(item.id, color)}
+              >
+                {item.id}: {item.hex}
+              </p>
+              {/* <button
+                className="border"
+                onClick={() => handleSave(item.id, color)}
+              >
+                save here
+              </button> */}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
